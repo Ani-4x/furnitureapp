7f5ae506c6fe117f80e9f4f0bda438cb117b87a3
Ani-4x <gawandeanimesh@gmail.com>
2024-12-04 03:34:06 +0530

    modified:   app/index.tsx 	new file:   navigation/BottomTabNavigation.jsx 	new file:   screens/Home.jsx 	modified:   screens/Search.jsx 	new file:   screens/index.js 	modified:   screens/profile.jsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,43 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts} from 'expo-font';
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+import { NavigationContainer, NavigationIndependentTree } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import BottomTabNavigation from '../navigation/BottomTabNavigation';
+
+const Stack = createNativeStackNavigator();
 
 export default function App () {
+
+   const [fontsLoaded] = useFonts({
+    
+   })
+
+   const onLayoutRootView = useCallback(async() => {
+    if (fontsLoaded){
+      await SplashScreen.hideAsync();
+    }
+   }, [fontsLoaded]);
+
+   if(!fontsLoaded){
+    return null;
+   }
+
     return (
-        <View style={styles.container} >
-            <Text>
-               ABCD
-            </Text>
-            <StatusBar style="auto"/>    
-       </View>
+      <NavigationIndependentTree>
+       <NavigationContainer>
+         <Stack.Navigator>
+          <Stack.Screen
+               name ="Bottom Navigation"
+        
+               component={BottomTabNavigation}
+               options={{headerShown:false}}
+          />
+         </Stack.Navigator>
+       </NavigationContainer>
+      </NavigationIndependentTree>
     );
 }
 
@@ -18,5 +47,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: "center",
         justifyContent: "center"
+    },
+    textStyle: {
+    
+      fontSize: 20
     }
 })
